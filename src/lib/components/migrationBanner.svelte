766a28a170efa79a30d03db43ca5f60371f8bcd9
JoLoZ <ev3mind@gmail.com>
2023-03-04 15:02:15 +0100

    Magic

---
+++ src/lib/components/migrationBanner.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import graphic from '$lib/images/backgrounds/chaos-flurry.svg';
+	import logo from '$lib/images/logos/chaos.svg';
+</script>
+
+<div class="banner">
+	<img class="logo" src={logo} title="Chaos Initiative" alt="Chaos Initiative" />
+	<div class="message">
+		Chaos Initiative is now Strata Source! <a href="/were-strata">Read the announcement</a>
+	</div>
+	<div class="filler" style="--url: url({graphic});" />
+</div>
+
+<style>
+	.banner {
+		height: 4rem;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		background: #191919;
+		color: white;
+	}
+	.logo {
+		height: 3rem;
+		padding: 1rem;
+	}
+	.filler {
+		height: 100%;
+		flex-grow: 1;
+		background-image: linear-gradient(90deg, #191919 0%, transparent 60%), var(--url);
+		background-size: cover;
+		background-position: center;
+	}
+	a {
+		color: #ce3d3d;
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import MigrationBanner from '$lib/components/migrationBanner.svelte';
+</script>
+
+<MigrationBanner />
+
+<slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
+
+	:global(body) {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
