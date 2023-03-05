257ee2ee43e4c521e92e3435857a7a8b96c90f52
Laura Sofia Heimann <hello@laura.media>
2023-03-06 00:23:48 +0100

    Frontpage code

--- src/lib/components/migrationBanner.svelte
+++ src/lib/components/migrationBanner.svelte
@@ -1,38 +1,34 @@
 <script lang="ts">
-	import graphic from '$lib/images/backgrounds/chaos-flurry.svg';
-	import logo from '$lib/images/logos/chaos.svg';
+	import StrataIconOnDarkMono from '$lib/images/logos/strata-icon-ondark-mono.svg';
 </script>
 
-<div class="banner">
-	<img class="logo" src={logo} title="Chaos Initiative" alt="Chaos Initiative" />
-	<div class="message">
-		Chaos Initiative is now Strata Source! <a href="/were-strata">Read the announcement</a>
-	</div>
-	<div class="filler" style="--url: url({graphic});" />
+<div class="migration-banner">
+	<img src={StrataIconOnDarkMono} alt="Strata Icon" />
+	<span>Chaos Initiative is now Strata Source!</span>
+	<a href="/hello-from-strata">Read the announcement</a>
 </div>
 
-<style>
-	.banner {
-		height: 4rem;
+<style lang="scss">
+	.migration-banner {
+		background: #a32b2b;
+		color: #fff;
 		width: 100%;
+		padding: 15px;
 		display: flex;
-		align-items: center;
-		background: #191919;
-		color: white;
-	}
-	.logo {
-		height: 3rem;
-		padding: 1rem;
-	}
-	.filler {
-		height: 100%;
-		flex-grow: 1;
-		background-image: linear-gradient(90deg, #191919 0%, transparent 60%), var(--url);
-		background-size: cover;
-		background-position: center;
-	}
-	a {
-		color: #ce3d3d;
-		text-decoration: none;
+		justify-content: center;
+		gap: 15px;
+
+		& img {
+			height: 20px;
+		}
+		& a {
+			color: #fff;
+			font-weight: bold;
+			transition: 0.2s ease-in-out all;
+
+			&:hover {
+				opacity: 0.6;
+			}
+		}
 	}
 </style>
