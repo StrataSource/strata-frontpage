8a1830b72418f00e4311316fac9dd0f23dc41bf5
JoLoZ <ev3mind@gmail.com>
2023-03-11 18:38:22 +0100

    Prototype for header of announcement

--- src/routes/the-future-of-chaos/+page.svelte
+++ src/routes/the-future-of-chaos/+page.svelte
@@ -1,6 +1,38 @@
 <script>
-	import Meta from "$lib/components/Meta.svelte";
+	import Meta from '$lib/components/Meta.svelte';
 
+	import chaosIcon from '$lib/images/logos/chaos.svg';
 </script>
-<Meta title="The future of Chaos" description="Chaos Initiative is evolving into something more, read more here."></Meta>
+
+<Meta
+	title="The future of Chaos"
+	description="Chaos Initiative is evolving into something more, read more here."
+/>
+
+<div class="hero">
+	<img src={chaosIcon} alt="Chaos Initiative" class="icon" />
+	<div class="mdi mdi-arrow-right icon" />
+	<img
+		src="https://branding.stratasource.org/i/strata/icon/ondark/mono.svg"
+		alt="Strata"
+		class="icon"
+	/>
+</div>
+
 <h1>Someone needs to write this</h1>
+
+<style lang="scss">
+	.hero {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 50px;
+
+		& .icon {
+			height: 5rem;
+			font-size: 4.5rem;
+			line-height: 1;
+			margin: 1.5rem;
+		}
+	}
+</style>
