d72709ded927db64901213010877a410ab3be3b6
JoLoZ <ev3mind@gmail.com>
2023-03-12 12:20:49 +0100

    Added Post blocks and made the announcement use them

--- src/routes/the-future-of-chaos/+page.svelte
+++ src/routes/the-future-of-chaos/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Meta from '$lib/components/Meta.svelte';
+	import PostImage from '$lib/components/PostImage.svelte';
+	import PostText from '$lib/components/PostText.svelte';
 
 	import chaosIcon from '$lib/images/logos/chaos.svg';
 </script>
@@ -19,7 +21,31 @@
 	/>
 </div>
 
-<h1>Someone needs to write this</h1>
+<PostText>
+	<div>
+		Hey everyone,
+		<br />
+		We are excited to announce that Chaos Initiative is evolving into something even greater! As we continue
+		to develop our own branch of the Source Engine, we have decided to rebrand ourselves as
+		<strong>Strata</strong>. This new name represents our hard work digging deep through the Source
+		Engine's code.
+		<br />
+		And while our name may be changing, we want to reassure you that our passion for building amazing
+		mods remains stronger than ever. We believe that the future of gaming is built on collaboration and
+		innovation, and we are excited to be a part of that future. With Strata, we will continue to push
+		the boundaries of what is possible, and we look forward to sharing our journey with all of you.
+		<br />
+		Thank you for your support and for being a part of this incredible community.
+		<br /><br />
+		Sincerely,
+		<br />
+		The Strata Team
+	</div>
+</PostText>
+<PostImage src="https://source.unsplash.com/random?dark" mode="parallax" />
+<PostText>
+	<div>This would be an excelent place for some Q&A after we finish this</div>
+</PostText>
 
 <style lang="scss">
 	.hero {
