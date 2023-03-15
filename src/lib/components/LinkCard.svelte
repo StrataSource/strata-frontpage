dbbd698afd91a91bd78c7c1b6746f73d30b714f4
JoLoZ <ev3mind@gmail.com>
2023-03-15 22:36:32 +0100

    Added link card

---
+++ src/lib/components/LinkCard.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import type { BrandColors } from '$lib/types';
+
+	export let theme: BrandColors;
+	export let background: string;
+	export let href: string;
+	export let title: string;
+	export let description: string;
+	export let cta = 'Learn more';
+</script>
+
+<a
+	class="card"
+	{href}
+	target={href.startsWith('/') ? '_self' : '_blank'}
+	style="--theme: {theme};--themeTransparent: {theme}44; --bg: url({background})"
+>
+	<div class="title">{title}</div>
+	<div class="description">{description}</div>
+	<div class="cta">{cta}</div>
+</a>
+
+<style lang="scss">
+	.card {
+		background-image: linear-gradient(0deg, var(--themeTransparent), var(--themeTransparent)),
+			linear-gradient(0deg, #00000099, #00000099), var(--bg);
+		background-position: center;
+		background-size: 100%;
+
+		display: block;
+		cursor: pointer;
+
+		border-bottom: var(--theme) 0.2rem solid;
+
+		color: white;
+		text-decoration: none;
+		padding: 1rem;
+
+		transition: 250ms;
+	}
+
+	.card:hover {
+		background-size: 110%;
+	}
+
+	.title {
+		font-size: 2rem;
+        margin-bottom: 0.5rem;
+	}
+
+	.cta {
+		border: 0.1rem white solid;
+		padding: 0.25rem;
+		width: fit-content;
+		margin-top: 1rem;
+		transition: 250ms;
+	}
+	.cta:hover {
+		background: white;
+		color: black;
+	}
+</style>
---
+++ src/lib/types.ts
@@ -0,0 +1,5 @@
+export enum BrandColors {
+	P2CE = '#ff7b02',
+	MOMENTUM = '#1896d3',
+	STRATA = '#f0413c'
+}
